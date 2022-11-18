2884b4002257ad414ab4bea7d0df9f29dc08918c
RashidBaghirov <rashidtb@code.edu.az>
2022-11-18 22:24:22 +0400

    my first class in C#

---
+++ Hieveryone/Program.cs
@@ -0,0 +1,47 @@
+﻿#region Task1
+//Console.WriteLine("Write one sentence");
+//string sentence=Console.ReadLine();
+//string sentence2 = sentence.ToUpper();
+//Console.Write("Result: ");
+//FirstletterUp(sentence2);
+
+//static void FirstletterUp(string sentence)
+//{
+//	for (int i = 0; i < sentence.Length; i++)
+//	{
+//		if (i == 0)
+//		{
+
+//			Console.Write(sentence[i]);
+//			sentence = sentence.ToLower();
+//			i++;
+//		}		if (sentence[i] == ' ')
+//		{
+//			sentence = sentence.ToUpper();
+//			Console.Write(sentence[i]);
+//			i++;
+//		}
+//		Console.Write(sentence[i]);
+//        sentence = sentence.ToLower();
+//    }
+
+//}
+
+#endregion
+
+
+#region Task2
+
+//using Hieveryone;
+
+
+//Student telebe=new Student("Narman","Novruzov");
+
+#endregion
+
+
+
+
+
+
+
---
+++ Hieveryone/Student.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hieveryone
+{
+    internal class Student
+    {
+
+        public string Name;
+        public string Surname;
+        public byte Group;
+        public byte Point;
+        public bool IsGreated;
+
+
+     
+        public Student(string name, string surname,byte group,byte point,bool isgreated)
+        {
+            Name=name;
+            Surname=surname;
+            Group=group;
+            Point=point;
+            IsGreated=isgreated;
+            Console.WriteLine(Name);
+            Console.WriteLine(Surname);
+            Console.WriteLine(Group);
+            Console.WriteLine(Point);
+
+            Greated();
+
+        }
+
+        public Student(string name,string surname)
+        {
+            Name = name;
+            Surname = surname;
+
+            Console.Write(Name+ ' ');
+            Console.WriteLine(Surname);
+        }
+
+
+        public void Greated()
+        {
+            if (IsGreated)
+            {
+                Console.WriteLine("Artiq mezun olub");
+            }
+            else
+            {
+                Console.WriteLine("Hal hazirda Tehsil alir");   
+            }
+        }
+
+
+        public void StudentPoint()
+        {
+            if (Point>80)
+            {
+                Console.WriteLine("Siz novbeti merheleye ugurla kecdiniz");
+            }
+            else
+            {
+                Console.WriteLine("Teesufler olsunki siz merheleni kece bilmediniz");
+               
+               
+            }
+        }
+
+       
+    }
+}
+
